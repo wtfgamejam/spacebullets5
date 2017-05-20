fedca68f38b779629a4df248ac23d4d629aa7848
Amesh Narsing <amesh@hololabs.org>
2017-05-19 23:50:08 -0700

    plugins and shit

---
+++ Assets/Scripts/PlaneFlip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlaneFlip : MonoBehaviour {
+
+	Vector3 rotation;
+	float rotationSpeed = 90f;
+
+	// Use this for initialization
+	void Start () {
+		rotation = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = rotation.x;
+		float y = rotation.y;
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			x += rotationSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			x -= rotationSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			y += rotationSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			y -= rotationSpeed;
+		}
+
+		rotation = new Vector3(x,y,0f);
+
+		transform.DOKill();
+		transform.DORotate(rotation, 0.5f, RotateMode.FastBeyond360);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 0.05f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = transform.localPosition;
+		float x = direction.x;
+		float y = direction.y;
+		if(Input.GetKey(KeyCode.W))
+		{
+			y += moveSpeed;
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			x -= moveSpeed;
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			y -= moveSpeed;
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			x += moveSpeed;
+		}
+
+		transform.localPosition = new Vector3(x,y,direction.z);
+	}
+}
