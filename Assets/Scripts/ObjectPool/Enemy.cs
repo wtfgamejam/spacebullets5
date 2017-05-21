955aea1fa18a12f4daf145dd920687d89b0045ee
Amesh Narsing <amesh@hololabs.org>
2017-05-21 13:58:36 -0700

    Rough ai

--- Assets/Scripts/ObjectPool/Enemy.cs
+++ Assets/Scripts/ObjectPool/Enemy.cs
@@ -3,12 +3,18 @@
 [RequireComponent(typeof(Rigidbody))]
 public class Enemy : PooledObject {
 
-	public float MoveSpeed = 15f;
+	public Material rageMaterial;
+
+	float MoveSpeed = 50f;
+	float RageSpeed = 15f;
 	public Rigidbody Body { get; private set; }
 
 	MeshRenderer[] meshRenderers;
 
 	GameObject player;
+	GameObject plane;
+
+	public bool attack=false;
 
 	public void SetMaterial (Material m) {
 		for (int i = 0; i < meshRenderers.Length; i++) {
@@ -21,25 +27,42 @@ public class Enemy : PooledObject {
 		meshRenderers = GetComponentsInChildren<MeshRenderer>();
 
 		player = GameObject.FindGameObjectWithTag("Player");
+		plane = GameObject.FindGameObjectWithTag("Plane");
+	}
+
+	void OnEnable()
+	{
+		attack = false;
 	}
 
 	void Update()
 	{
-		Body.transform.LookAt(player.transform.position);
-		Body.position += transform.forward*MoveSpeed*Time.deltaTime;
+		if(attack)
+		{
+			Body.transform.LookAt(player.transform.position);
+			Body.position += transform.forward*RageSpeed*Time.deltaTime;
+		}
+		else
+		{
+			Body.transform.LookAt(plane.transform.position);
+			Body.position += transform.forward*MoveSpeed*Time.deltaTime;
+		}
 	}
 
 	void OnTriggerEnter (Collider enteredCollider) {
 		if (enteredCollider.CompareTag("Player")) {
 			ReturnToPool();
 		}
+		if (enteredCollider.CompareTag("Plane")) {
+			Debug.Log("Hit plane, ATTACK!");
+			attack = true;
+			SetMaterial(rageMaterial);
+			Body.velocity = Vector3.zero;
+		}
 	}
 
 	void OnParticleCollision (GameObject other)
 	{
-//		if (col.collider.CompareTag("Kill Zone")) {
-//			ReturnToPool();
-//		}
 		ReturnToPool();
 		Debug.Log("hit by bullet");
 	}
