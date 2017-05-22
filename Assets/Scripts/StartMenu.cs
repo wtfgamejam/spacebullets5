6816d039e6195c4c61fe7ce62dfdebc5dff19dba
Amesh Narsing <amesh@hololabs.org>
2017-05-22 12:24:34 -0700

    Added main music.

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -6,6 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
 
+	void Start()
+	{
+		if (!GameObject.Find("AudioManager"))
+		{
+			GameObject mainAudio = new GameObject();
+			mainAudio.name = "AudioManager";
+			mainAudio.transform.SetParent(transform.parent);
+			AudioSource audio = mainAudio.AddComponent<AudioSource>();
+
+			AudioClip main = Resources.Load<AudioClip>("Audio/mainTheme");
+			audio.clip = main;
+			audio.loop = true;
+			audio.Play();
+			DontDestroyOnLoad(mainAudio);
+		}
+	}
+
 	public void StartGame()
 	{
 		SceneManager.LoadScene("Main");
